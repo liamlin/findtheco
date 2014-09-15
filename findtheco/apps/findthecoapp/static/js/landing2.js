58f208fa188739dd8199d8dbe42ae53dcfa192a0
Willie Tran <tran.william26@gmail.com>
2014-09-14 23:45:41 -0700

    modded UGA, made list text more readable, and removed SendGrid AJAX call

--- findtheco/apps/findthecoapp/static/js/landing2.js
+++ findtheco/apps/findthecoapp/static/js/landing2.js
@@ -3,8 +3,6 @@
  */
 $(document).ready(function() {
 
-
-
     function validateEmail(email) {
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
@@ -27,31 +25,8 @@ $(document).ready(function() {
         }
     }
 
-    // Post request we need to make to add an e-mail to our list:
-    // curl -X POST https://sendgrid.com/api/newsletter/lists/email/add.json -d api_user=findtheco -d api_key=XXXXXXXX -d list=FindTheCoPre-launchList -d data={"email":"willie@gmail.com"}
-
     $('.submit-btn').on('click', function(){
         validate();
-        var url = "https://sendgrid.com/api/newsletter/lists/email/add.json";
-        var api_user = "findtheco";
-        var api_key = "willie1026";
-        var list = "FindTheCoPre-launchList";
-        var data = {"email":$('.email-form').val()};
-        var emailInfo = {"api_user": api_user, "api_key": api_key, "list":list, "data": data};
-        var emailInfoJSON = JSON.stringify(emailInfo);
-        $.ajax ({
-            url: url,
-            type: "POST",
-            crossDomain: true,
-            dataType: "json",
-            data: emailInfoJSON,
-            success: function(response) {
-                console.log(response);
-            },
-            error: function(response) {
-                console.log("This is not good");
-            }
-        });
         return false;
     })
 });
